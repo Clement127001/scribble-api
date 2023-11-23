e6da2c877102beb9c1a8a797b7f920cf5e31fe36
Clement127001 <babiyonclement18@gmail.com>
2023-11-23 19:19:19 +0530

    add controller logic for get project details

--- controller/projects.js
+++ controller/projects.js
@@ -1,5 +1,7 @@
 const { StatusCodes } = require("http-status-codes");
+const { NotFoundError } = require("../errors");
 const Project = require("../model/projects");
+const User = require("../model/user");
 
 const getAllProjects = async (req, res) => {
   res.send("Get all projects");
@@ -7,7 +9,34 @@ const getAllProjects = async (req, res) => {
 
 const getProject = async (req, res) => {
   const { id: projectId } = req.params;
-  res.send(`get single project with id : ${projectId}`);
+  const project = await Project.findOne({ _id: projectId });
+
+  if (!project) throw new NotFoundError("Project not found");
+
+  const { createdBy } = project;
+
+  const foundUser = await User.findOne({ _id: createdBy });
+
+  // console.log(foundUser);
+
+  const { _id: userId, profile, name: username, role } = foundUser;
+
+  const user = { userId, username, profile, role };
+
+  const foundMoreProjects = await Project.find({ createdBy });
+
+  const moreProjects = foundMoreProjects.map((proj) => {
+    if (proj._id == projectId) {
+      return;
+    }
+    return {
+      images: proj.images,
+      projectId: proj._id,
+      category: proj.category,
+    };
+  });
+
+  res.status(StatusCodes.OK).json({ project, user, moreProjects });
 };
 
 const createNewProject = async (req, res) => {
